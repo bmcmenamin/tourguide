1dae557eee0d817eec2edac892a9ecf44394bacf
Brenton McMenamin <bmcmenamin@gmail.com>
2019-05-21 23:56:19 +0100

    Update gcs_to_sql.sh

--- wikidump_scripts/gcs_to_sql.sh
+++ wikidump_scripts/gcs_to_sql.sh
@@ -13,17 +13,39 @@ ACCESS_TOKEN="$(gcloud auth application-default print-access-token)"
 PROJECT_ID="$(gcloud config list --format 'value(core.project)' 2>/dev/null)"
 
 
-DBNAME=wikidump${DUMPDATE}
-SQLINSTANCE=wikidump
+DBNAME=wiki-${DUMPDATE}
+SQLINSTANCE=bmcdb
 
 
 gcloud sql databases create ${DBNAME} --instance=${SQLINSTANCE}
 
-for FILE in $REDIRECT_FILE $PAGES_FILE $CATEGORY_FILE $GEO_FILE $LINK_FILE 
-do
-    gcloud sql import sql \
-        $SQLINSTANCE \
-        gs://${PROJECT_ID}.appspot.com/wikidump/${FILE} \
-        --quiet \
-        --database=${DBNAME}
-done
+
+gcloud sql import sql \
+    $SQLINSTANCE \
+    gs://${PROJECT_ID}.appspot.com/wikidump/${GEO_FILE} \
+    --quiet \
+    --database=${DBNAME}
+
+gcloud sql import sql \
+    $SQLINSTANCE \
+    gs://${PROJECT_ID}.appspot.com/wikidump/${REDIRECT_FILE} \
+    --quiet \
+    --database=${DBNAME}
+
+gcloud sql import sql \
+    $SQLINSTANCE \
+    gs://${PROJECT_ID}.appspot.com/wikidump/${PAGES_FILE} \
+    --quiet \
+    --database=${DBNAME}
+
+gcloud sql import sql \
+    $SQLINSTANCE \
+    gs://${PROJECT_ID}.appspot.com/wikidump/${CATEGORY_FILE} \
+    --quiet \
+    --database=${DBNAME}
+
+gcloud sql import sql \
+    $SQLINSTANCE \
+    gs://${PROJECT_ID}.appspot.com/wikidump/${LINK_FILE} \
+    --quiet \
+    --database=${DBNAME}
