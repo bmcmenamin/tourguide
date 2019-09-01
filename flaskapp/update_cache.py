64dc5beea09954db43eb72fc1167f06d33baa10c
bmcmenamin <bmcmenamin@gmail.com>
2019-09-01 13:07:57 -0400

    update gunicorn timeout

--- flaskapp/update_cache.py
+++ flaskapp/update_cache.py
@@ -11,36 +11,37 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.secret_key = "OHnM3KAkTEhsI&j6"
 
 
-BATCH_SIZE = 200
+BATCH_SIZE = 100
 MAX_PAGES = 10000
 
 
+def _run_throuh_cacherfuncs(page_titles):
+    wi.OutLinkFinder().get_payload(page_titles)
+    wi.InLinkFinder().get_payload(page_titles)
+    wi.CategoryFinder().get_payload(page_titles)
+
+
 @app.route('/update-popular', methods=['GET'])
-def run_update(batch_size=BATCH_SIZE):
+def run_update_popular(batch_size=BATCH_SIZE):
 
     for offset in range(0, MAX_PAGES, batch_size):
         page_titles = wi.MostViewedFinder().get_payload(batch_size, offset)
-        wi.OutLinkFinder().get_payload(page_titles)
-        wi.InLinkFinder().get_payload(page_titles)
-        wi.CategoryFinder().get_payload(page_titles)
+        _run_throuh_cacherfuncs(page_titles)
 
     return make_response(jsonify(page_titles), 200)
 
 
 @app.route('/update-random', methods=['GET'])
-def run_update(batch_size=BATCH_SIZE):
+def run_update_random(batch_size=BATCH_SIZE):
     page_titles = wi.RandomPageFinder().get_payload(batch_size)
-    wi.OutLinkFinder().get_payload(page_titles)
-    wi.InLinkFinder().get_payload(page_titles)
-    wi.CategoryFinder().get_payload(page_titles)
+    _run_throuh_cacherfuncs(page_titles)
 
     return make_response(jsonify(page_titles), 200)
-
 
 
 if __name__ == '__main__':
     app.run(
         host='127.0.0.1',
-        port=8080,
+        port=24601,
         debug=True
     )
