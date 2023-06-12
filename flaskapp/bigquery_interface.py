e76f1cb4495052f9dfa6b480823bbee642c00414
bmcmenamin <bmcmenamin@gmail.com>
2023-06-11 20:36:05 -0400

    setting up test notebook

--- flaskapp/bigquery_interface.py
+++ flaskapp/bigquery_interface.py
@@ -7,14 +7,16 @@ import pandas as pd
 QUERY_EDGES = """
 WITH edges AS (
     SELECT
-        ARRAY_CONCAT(
-            ARRAY(SELECT AS STRUCT il AS from_node, page_id AS to_node FROM UNNEST(in_links) AS il)
-            , ARRAY(SELECT AS STRUCT page_id AS from_node, ol AS to_node FROM UNNEST(out_links) AS ol)
+        ARRAY(
+            SELECT AS STRUCT
+                page_id AS from_node
+                , ol AS to_node
+            FROM UNNEST(out_links) AS ol
+            WHERE ol IN UNNEST(@nodes)
         ) AS edge_array
     FROM `pages.links`
     WHERE
         page_id IN UNNEST(@nodes)
-        AND (num_in_links + num_out_links) > 1
 )
 
 SELECT DISTINCT
@@ -23,11 +25,49 @@ SELECT DISTINCT
 FROM
     edges
     , UNNEST(edge_array) AS e
-    JOIN `pages.links` AS l0 ON l0.page_id = e.from_node
-    JOIN `pages.links` AS l1 ON l1.page_id = e.to_node
-WHERE
-    (l0.num_in_links + l0.num_out_links) > 1
-    AND (l1.num_in_links + l1.num_out_links) > 1
+"""
+
+
+QUERY_NEIGHBORS = """
+WITH
+
+current_nodes AS (
+    SELECT
+        page_id
+        , CASE
+            WHEN num_in_links < @max_in_degree THEN in_links
+            ELSE []
+            END AS in_links
+        , CASE
+            WHEN num_out_links < @max_out_degree THEN out_links
+            ELSE []
+            END AS out_links
+    FROM
+        `pages.links`
+    WHERE
+        page_id IN UNNEST(@nodes)
+)
+
+, new_neighbors AS (
+    SELECT DISTINCT
+        new_page_id AS page_id
+    FROM
+        current_nodes
+        , UNNEST(ARRAY_CONCAT(in_links, out_links)) AS new_page_id
+    WHERE
+        new_page_id NOT IN UNNEST(@nodes)
+)
+
+SELECT
+    page_id
+    , title
+    , has_place_category
+    , num_in_links
+    , num_out_links
+    , num_in_links + num_out_links AS degree
+FROM
+    `pages.links`
+    JOIN new_neighbors USING(page_id)
 """
 
 
@@ -60,33 +100,37 @@ QUALIFY ROW_NUMBER() OVER (ORDER BY d ASC) <= @num_to_keep;
 
 
 QUERY_NEARBY_EXTENDED = """
-WITH nested_neighbors AS (
+WITH new_neighbors AS (
     SELECT
-        page_id
-        , ARRAY_CONCAT(in_links, out_links) AS new_edges
-    FROM `pages.links`
+        page_id AS orig_page_id
+        , new_page_id
+    FROM
+        `pages.links`
+        , UNNEST(ARRAY_CONCAT(in_links, out_links)) AS new_page_id
     WHERE
         page_id IN UNNEST(@nodes)
+        AND new_page_id NOT IN UNNEST(@nodes)
 )
 
-, neighbors AS (
+, filtered_neighbors AS (
     SELECT
-        n.page_id AS geo_page_id
-        , e AS new_page_id
+        orig_page_id
+        , new_page_id
     FROM
-        nested_neighbors AS n
-        , UNNEST(new_edges) AS e
-        JOIN `pages.links` AS l ON e = l.page_id
+        new_neighbors AS n
+        JOIN `pages.links` AS l ON l.page_id = new_page_id
+        LEFT JOIN `pages.geotags` AS gt ON gt.page_id = new_page_id
     WHERE
-        l.num_in_links < @max_in_degree
-        AND l.has_place_category
+        (l.has_place_category OR gt.page_id IS NOT NULL)
+        AND l.num_in_links < @max_in_degree
+        AND l.num_out_links < @max_out_degree
 )
 
 SELECT
     new_page_id AS page_id
-FROM neighbors
+FROM filtered_neighbors
 GROUP BY new_page_id
-HAVING COUNT(DISTINCT geo_page_id) > 1
+HAVING COUNT(DISTINCT orig_page_id) > 1
 """
 
 
@@ -148,10 +192,23 @@ def query_nearby_extended(bq: BigQueryInterface, nodes: List[int]) -> pd.DataFra
     job_config = bigquery.QueryJobConfig(
         query_parameters=[
             bigquery.ArrayQueryParameter("nodes", "INT64", nodes),
-            bigquery.ScalarQueryParameter("max_in_degree", "INT64", 2000)
+            bigquery.ScalarQueryParameter("max_in_degree", "INT64", 1000),
+            bigquery.ScalarQueryParameter("max_out_degree", "INT64", 1000)
         ]
     )
 
     return bq.query(QUERY_NEARBY_EXTENDED, job_config=job_config)
 
+
+def query_neighbors(bq: BigQueryInterface, nodes: List[int]) -> pd.DataFrame:
+
+    job_config = bigquery.QueryJobConfig(
+        query_parameters=[
+            bigquery.ArrayQueryParameter("nodes", "INT64", nodes),
+            bigquery.ScalarQueryParameter("max_in_degree", "INT64", 1000),
+            bigquery.ScalarQueryParameter("max_out_degree", "INT64", 1000)
+        ]
+    )
+
+    return bq.query(QUERY_NEIGHBORS, job_config=job_config)
 
